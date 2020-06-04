50fb777924680be8a546190e3c908ca442529536
Lukasz Kucia <lukaszmpakietm3@gmail.com>
2020-06-04 20:10:50 +0200

    Add version 1.0 beta and STL new jog

--- CDJ100MIDI.ino
+++ CDJ100MIDI.ino
@@ -1,59 +1,62 @@
+//  -------------------------------- ABOUT ---------------------------------------
+// | Project: Pioneer CDJ 100 MIDI                                               |
+// | Autor: Łukasz Kucia                                                         |
+// | Version : 1.0 Beta                                                          |
+// | Description: Change old cd player on midi controller works with dj programs |
+// |             (Traktor, Virtual DJ, Serato, FL studio etc.)                   |
+// | Github: https://github.com/Lukaszm328/ArduinoMidiControllerUsb              | 
+//  ------------------------------------------------------------------------------ 
 #define USE_ROTARY_ENCODER
 #include <MIDI_Controller.h>
 
-const uint8_t velocity = 0b1111111; // Maximum velocity (0b1111111 = 0x7F = 127)
-const uint8_t Controller = 0x14; // MIDI Control Change controller number
-const int speedMultiply = 1;     // No change in speed of the encoder (number of steps is multiplied by 1)
+// --------------------------------------------- VARIABLES ----------------------------------------------------
+const uint8_t velocity = 0b1111111;  // Maximum velocity (0b1111111 = 0x7F = 127)                             |
+const uint8_t Controller = 0x14;     // MIDI Control Change controller number                                 |
+const int speedMultiply = 1;         // No change in speed of the encoder (number of steps is multiplied by 1)|
+const int midiLedCommunication = 48; // Setup the MIDI communication, with an LED on pin 48                   |
+// ------------------------------------------------------------------------------------------------------------
 
-// ------------------------- DECK A ------------------------------
+// ------------------------ BUTTONS ----------------------------
+  Digital Button_PLAY(1, 60, 1, velocity);           // PIN 1  |  
+  Digital Button_CUE(0, 61, 1, velocity);            // PIN 0  |
+  Digital Button_FORWARD(2, 62, 1, velocity);        // PIN 2  |
+  Digital Button_BACKWARDS(3, 63, 1, velocity);      // PIN 3  |
+  Digital Button_NEXT(4, 64, 1, velocity);           // PIN 4  |
+  Digital Button_PREVIOUS(5, 65, 1, velocity);       // PIN 5  |
+  Digital Button_JET(6, 66, 1, velocity);            // PIN 6  |
+  Digital Button_ZIP(7, 67, 1, velocity);            // PIN 7  |
+  Digital Button_WAH(8, 68, 1, velocity);            // PIN 8  |
+  Digital Button_HOLD(9, 69, 1, velocity);           // PIN 9  |
+  //Digital Button_TIME(10, 70, 1, velocity);          // PIN 10 |
+  Digital Button_EJECT(10, 71, 1, velocity);         // PIN 11 |
+  Digital Button_TEMPO(14, 72, 1, velocity);         // PIN 12 |
+  Digital Button_ENCODER_BT(16, 73, 1, velocity);    // PIN 13 |
+// -------------------------------------------------------------
 
-// Buttons
-Digital A_Button_PLAY(22, 60, 1, velocity);
-Digital A_Button_CUE(24, 61, 1, velocity);
-Digital A_Button_FORWARD(26, 62, 1, velocity);
-Digital A_Button_BACKWARDS(28, 63, 1, velocity);
-Digital A_Button_NEXT(30, 64, 1, velocity);
-Digital A_Button_PREVIOUS(32, 65, 1, velocity);
-Digital A_Button_JET(34, 66, 1, velocity);
-Digital A_Button_ZIP(36, 67, 1, velocity);
-Digital A_Button_WAH(38, 68, 1, velocity);
-Digital A_Button_HOLD(40, 69, 1, velocity);
-Digital A_Button_TIME(42, 70, 1, velocity);
-Digital A_Button_EJECT(44, 71, 1, velocity);
-Digital A_Button_TEMPO(46, 72, 1, velocity);
+// ------------------- ROTARY  ENCODERS -----------------------------------------------------
+// MODE: 7Fh/01h || TWOS_COMPLEMENT || POS1_NEG127                                          |
+// Set pulsesPerStep to 4, for normal rotary encoders  1, for jog wheels.                   |
+// JOG                                                                                      |
+  RotaryEncoder encoderBrowser(15, 18, 0x15, 1, speedMultiply, JOG, ADD_64);   // PIN 14, 15|
+// BROWSER                                                                                  |
+  RotaryEncoder encoderJOG(19, 20, Controller, 1, speedMultiply, JOG, ADD_64); // PIN 16, 17|
+// ------------------------------------------------------------------------------------------
 
-// Encoder jog
-RotaryEncoder encoderA(48, 50, Controller, 1, speedMultiply, JOG, TWOS_COMPLEMENT);
+// -------------------------- ANALOG --------------------------------- 
+ // TEMPO VALUE                                                      |
+Analog potentiometerTEMPO(A3, MIDI_CC::Channel_Volume, 1); // PIN A3 |
+// -------------------------------------------------------------------
 
-// Pitch fader
-Analog potentiometerA(A0, MIDI_CC::Channel_Volume, 1);
+// --------------------------- LED -----------------------------------
+   // PLAY/PAUSE                                                  // |
+                                                                  // |
+// -------------------------------------------------------------------
 
-// Led's
+// ------------------------- SETUP -----------------------------------
 
-// ------------------------- DECK B ------------------------------
-
-// Buttons
-Digital B_Button_PLAY(23, 73, 2, velocity);
-Digital B_Button_CUE(25, 74, 2, velocity);
-Digital B_Button_FORWARD(27, 75, 2, velocity);
-Digital B_Button_BACKWARDS(29, 76, 2, velocity);
-Digital B_Button_NEXT(31, 77, 2, velocity);
-Digital B_Button_PREVIOUS(33, 78, 2, velocity);
-Digital B_Button_JET(35, 79, 2, velocity);
-Digital B_Button_ZIP(37, 80, 2, velocity);
-Digital B_Button_WAH(39, 81, 2, velocity);
-Digital B_Button_HOLD(41, 82, 2, velocity);
-Digital B_Button_TIME(43, 83, 2, velocity);
-Digital B_Button_EJECT(45, 84, 2, velocity);
-Digital B_Button_TEMPO(47, 85, 2, velocity);
-
-// Encoder JOG
-RotaryEncoder encoderB(49, 51, Controller, 2, speedMultiply, JOG, TWOS_COMPLEMENT);
-
-// Pitch fader
-Analog potentiometerB(A1, MIDI_CC::Channel_Volume, 2);
-
-void setup() {}
+void setup() {
+  // Setup the MIDI communication, with an LED on pin.
+}
 
 void loop() {
   MIDI_Controller.refresh();
